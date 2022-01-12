b36fd1c200754fb0605535063b1805ef20a99aa4
happai079 <gksyddl22@gmail.com>
2022-01-12 11:53:22 +0900

    spring boot - mybatis 연동

---
+++ myboot2/src/test/java/com/multi/myboot2/Myboot2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.multi.myboot2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Myboot2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
