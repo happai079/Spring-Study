1342a4739ae1c11676bb838c354a8f48894c2f6c
happai079 <gksyddl22@gmail.com>
2022-01-12 14:50:29 +0900

    [spring MVC]도서 관리 프로그램 - 주석 한글 깨짐 해결

--- spring_mvc_mybatis_book/src/main/java/com/spring_mvc_mybatis/book/service/IBookService.java
+++ spring_mvc_mybatis_book/src/main/java/com/spring_mvc_mybatis/book/service/IBookService.java
@@ -6,7 +6,6 @@ import java.util.HashMap;
 import com.spring_mvc_mybatis.book.model.BookVO;
 
 public interface IBookService {
-	// 구현 클래스에서 오버라이딩할 추상 메소드 선언
 	ArrayList<BookVO> listAllBook();
 	void insertBook(BookVO bookVo);
 	void updateBook(BookVO bookVo);
