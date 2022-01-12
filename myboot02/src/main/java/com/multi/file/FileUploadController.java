2f9c78236c348cdc23e2b6c0514e0c04e0a3ab20
happai079 <gksyddl22@gmail.com>
2022-01-12 16:37:48 +0900

    [spring boot]파일 업로드 1 - UUID를 사용한 업로드 예제

--- myboot02/src/main/java/com/multi/file/FileUploadController.java
+++ myboot02/src/main/java/com/multi/file/FileUploadController.java
@@ -1,7 +1,14 @@
 package com.multi.file;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.UUID;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 public class FileUploadController {
@@ -9,5 +16,32 @@ public class FileUploadController {
 	@RequestMapping("/fileUploadForm")
 	public String fileUploadForm() {
 		return "upload/fileUploadForm";
+	}
+	
+	// 파일 업로드 처리
+	@RequestMapping("/fileUpload")
+	public String fileUpload(@RequestParam("uploadFile") MultipartFile file, Model model) throws IOException {
+		String savedFileName = "";
+		
+		// 1. 파일 저장 경로 설정: 실제 서비스되는 위치(프로젝트 외부에 저장)
+		String uploadPath = "C:/spring-workspace/upload/";
+		
+		// 2. 원본 파일 이름 알아오기
+		String originalFileName = file.getOriginalFilename();
+		
+		// 3. 파일 이름 중복되지 않도록 이름 변경 : 서버에 저장할 이름 UUID 사용
+		UUID uuid = UUID.randomUUID();
+		savedFileName = uuid.toString() + "_" + originalFileName;
+		
+		// 4. 파일 생성
+		File file1 = new File(uploadPath + savedFileName);
+		
+		// 5. 서버로 전송
+		file.transferTo(file1);
+		
+		// model로 저장
+		model.addAttribute("originalFileName", originalFileName);
+		
+		return "upload/fileUploadResult";
 	}
 }
---
+++ myboot02/src/main/webapp/WEB-INF/views/upload/fileUploadResult.jsp
@@ -0,0 +1,15 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>파일 업로드 결과</title>
+	</head>
+	<body>
+		<h3>파일 업로드 결과</h3>
+		${ originalFileName }파일을 업로드 하였습니다.<br>
+		(C:\spring-workspace\upload 확인) 
+	</body>
+</html>
