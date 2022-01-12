2c79b104418d2b5a93a53b56f56597cef13061f8
happai079 <gksyddl22@gmail.com>
2022-01-12 13:44:24 +0900

    spring boot - '@ComponentScan' '@MapperScan' 어노테이션 추가

--- myboot02/src/main/java/com/multi/myboot02/Myboot02Application.java
+++ myboot02/src/main/java/com/multi/myboot02/Myboot02Application.java
@@ -1,9 +1,16 @@
 package com.multi.myboot02;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+import com.multi.product.IProductDAO;
+import com.multi.product.ProductController;
 
 @SpringBootApplication
+@ComponentScan(basePackageClasses = ProductController.class)
+@MapperScan(basePackageClasses = IProductDAO.class)
 public class Myboot02Application {
 
 	public static void main(String[] args) {
