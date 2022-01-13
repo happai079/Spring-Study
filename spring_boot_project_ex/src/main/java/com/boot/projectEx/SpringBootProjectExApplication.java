2ae4bb6428e4129c99ef9685e4dc7c1119de523a
happai079 <gksyddl22@gmail.com>
2022-01-13 17:34:30 +0900

    [spring boot]샘플 웹 프로젝트 - 상품 VO, DAO, Service, Controller, Mapper 클래스 추가

--- spring_boot_project_ex/src/main/java/com/boot/projectEx/SpringBootProjectExApplication.java
+++ spring_boot_project_ex/src/main/java/com/boot/projectEx/SpringBootProjectExApplication.java
@@ -6,12 +6,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 import com.boot.projectEx.controller.MemberController;
+import com.boot.projectEx.controller.ProductController;
 import com.boot.projectEx.dao.IMemberDAO;
+import com.boot.projectEx.dao.IProductDAO;
 
 @SpringBootApplication
 @ComponentScan(basePackageClasses = MainController.class)
 @ComponentScan(basePackageClasses = MemberController.class)
+@ComponentScan(basePackageClasses = ProductController.class)
 @MapperScan(basePackageClasses = IMemberDAO.class)
+@MapperScan(basePackageClasses = IProductDAO.class)
 public class SpringBootProjectExApplication {
 
 	public static void main(String[] args) {
