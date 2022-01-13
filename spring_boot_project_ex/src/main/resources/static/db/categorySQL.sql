53debc074880f8f0ef6bf2cc74dc10c063ea03cc
happai079 <gksyddl22@gmail.com>
2022-01-13 17:17:05 +0900

    [spring boot]샘플 웹 프로젝트 - categoryDB, productDB 생성 및 데이터 입력

---
+++ spring_boot_project_ex/src/main/resources/static/db/categorySQL.sql
@@ -0,0 +1,9 @@
+CREATE TABLE category (
+		ctgId VARCHAR(2) NOT NULL PRIMARY KEY,
+		ctgName VARCHAR(30)
+);
+
+INSERT INTO category VALUES('1', '노트북');
+INSERT INTO category VALUES('2', '프린터');
+INSERT INTO category VALUES('3', '마우스');
+INSERT INTO category VALUES('4', '키보드');
