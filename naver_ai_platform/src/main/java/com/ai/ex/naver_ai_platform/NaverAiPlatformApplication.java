aaedc2e1dfc9e8baef3846e2b53c41e6c876b15a
happai079 <gksyddl22@gmail.com>
2022-01-21 10:22:38 +0900

    [naver ai platform] 네이버 AI 플랫폼 실습 프로젝트 생성

---
+++ naver_ai_platform/src/main/java/com/ai/ex/naver_ai_platform/NaverAiPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.ai.ex.naver_ai_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaverAiPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NaverAiPlatformApplication.class, args);
+	}
+
+}
